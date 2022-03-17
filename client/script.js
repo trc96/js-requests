5681d7bc3151c2b4925086c49604dc8955f2520f
trc96 <toddcrandall1996@gmail.com>
2022-03-17 16:10:20 -0600

    Completed Project

--- client/script.js
+++ client/script.js
@@ -10,7 +10,7 @@
 */
 
 // CODE HERE
-const sayHelloButton = document.querySelector('#say-hello-betton');
+const sayHelloButton = document.querySelector('#say-hello-button');
 
 
 // PROBLEM 2
@@ -40,6 +40,10 @@ sayHelloButton.addEventListener('mouseover', () => {
 */
 
 // CODE HERE
+sayHelloButton.addEventListener('mouseout', () => {
+    sayHelloButton.style.backgroundColor = '#EFEFEF';
+    sayHelloButton.style.color = 'black';
+})
 
 
 // PROBLEM 4
@@ -62,6 +66,8 @@ const sayHello = () => {
 
 // CODE HERE
 
+sayHelloButton.addEventListener('click', sayHello);
+
 
 // PROBLEM 5 
 /*
@@ -71,11 +77,18 @@ const sayHello = () => {
     
     Use axios inside the ohMy function to make a GET request to 'http://localhost:3000/animals' 
     
-    Handle the promise that's returned with a .then, which you should pass a callback function to. Inside the callback function, console.log the response's data (in the intermediate instructions we'll come back to this function and add HTML).
+    Handle the promise that's returned with a .then, which you should pass a callback function to. 
+    Inside the callback function, console.log the response's data (in the intermediate instructions 
+        we'll come back to this function and add HTML).
 */ 
 
 const ohMy = () => {
     // YOUR CODE HERE
+    axios.get(`http://localhost:3000/animals`)
+        .then(res => {
+            console.log(res.data)
+            return res.data
+        })
 }
 
 document.getElementById('animals-button').addEventListener('click', ohMy)
@@ -83,26 +96,39 @@ document.getElementById('animals-button').addEventListener('click', ohMy)
 
 // PROBLEM 6 
 /*
-    Now lets see if you can send a request param! inside repeatMyParam function below  make get request to 'http://localhost:3000/repeat/{SOMEPARAM}', but with a string instead of {SOMEPARAM}.  
+    Now lets see if you can send a request param! inside repeatMyParam function below  
+    make get request to 'http://localhost:3000/repeat/{SOMEPARAM}', but with a string instead of {SOMEPARAM}.  
 
     The function that runs when this request is made will return whatever parameter you sent 
 
-    Handle the promise returned from the request with a .then, which will take in a callback -- the callback function should print the response.data.
+    Handle the promise returned from the request with a .then, which will take in a callback -- 
+    the callback function should print the response.data.
     
-    Outside of the function, select the button with the id "repeat-button" and add a click event listener that calls the repeatMyParam function.
+    Outside of the function, select the button with the id "repeat-button" and add a click event 
+    listener that calls the repeatMyParam function.
     
     We'll be updating this function in the next problem.
 */
 
 const repeatMyParam = () => {
     //YOUR CODE HERE
-}
+    let repeatElement = document.getElementById('repeat-text');
+    axios.get(`http://localhost:3000/repeat/someParam`)
+    .then(res => {
+        repeatElement.textContent = res.data;
+        repeatElement.style.display = 'block';
+        console.log(res.data) 
+    })
 
+    
+}
+document.getElementById('repeat-button').addEventListener('click', repeatMyParam)
 // PROBLEM 7
 /*
     Now that we have the response data, let's add it to our web page! 
     
-    Inside the repeatMyParam function above, grab the element with the id of 'repeat-text' and set its textContent property equal to the response data.
+    Inside the repeatMyParam function above, grab the element with the id of 'repeat-text' and set its 
+    textContent property equal to the response data.
 */
 
 // Code in the repeatMyParam function above
@@ -113,13 +139,19 @@ const repeatMyParam = () => {
 /*
     Time to attach a query to our request!
 
-    Write a function that makes a get request to 'http://localhost:3000/query-test', with a query of your choice on the end!
+    Write a function that makes a get request to 'http://localhost:3000/query-test', with a query of your 
+    choice on the end!
 
-    Outside of your new function, select the button with the id "query-button" and add a click event listener that calls your function.
+    Outside of your new function, select the button with the id "query-button" and add a click event listener 
+    that calls your function.
 */
 
 // CODE HERE
-
+const myGetRequest = () => {
+    axios.get(`http://localhost:3000/?dontStopBelieving=Journey`)
+        // .then(({ data }) => console.log(data))
+}
+document.getElementById('query-button').addEventListener('click', myGetRequest)
 
 
 ////////////////
@@ -128,9 +160,11 @@ const repeatMyParam = () => {
 
 // PROBLEM 9
 /* 
-    Back in the ohMy function on Problem 5, replace the console log in the promise's callback with a for loop that loops over res.data. 
+    Back in the ohMy function on Problem 5, replace the console log in the promise's callback with a for 
+    loop that loops over res.data. 
 
-    On each iteration of the loop, create a new p element. Set its textContent equal the string at the current index (i) and then append the new p element onto the document's body. 
+    On each iteration of the loop, create a new p element. Set its textContent equal the string at the 
+    current index (i) and then append the new p element onto the document's body. 
 */
 
 // Code in the ohMy function in Problem 5
@@ -154,21 +188,31 @@ const repeatMyParam = () => {
 
 //PROBLEM 11
 /*
-    You are going to add the ability to POST to the server. You'll need to create a small form and write a function that makes a post request. Then you'll attach that function to the submit event on the form. We'll be creating a list of foods. 
+    You are going to add the ability to POST to the server. You'll need to create a small form and write a 
+    function that makes a post request. Then you'll attach that function to the submit event on the form. 
+    We'll be creating a list of foods. 
 
-    In the index.html file inside of the client folder, create a form with one text input field and a button. The input field should have a placeholder that tells the user to enter a food. And the button should indicate that it will add food into a list. 
+    In the index.html file inside of the client folder, create a form with one text input field and a 
+    button. The input field should have a placeholder that tells the user to enter a food. And the button 
+    should indicate that it will add food into a list. 
 
     In this file (script.js), create a function called createFood. 
     
-    Inside the function, select the input you just created in the HTML and save it to a variable called foodInput. 
+    Inside the function, select the input you just created in the HTML and save it to a variable called 
+    foodInput. 
     
-    Next, create an object called body inside the function. It should have one key-value pair. The key should be newFood (make sure to match the case and spelling exactly) and the value should be the value of the food input. 
+    Next, create an object called body inside the function. It should have one key-value pair. The key 
+    should be newFood (make sure to match the case and spelling exactly) and the value should be the value 
+    of the food input. 
 
-    Now make an axios post request to /food. Inside the parentheses where you passed the URL in, pass in body as the second argument. 
+    Now make an axios post request to /food. Inside the parentheses where you passed the URL in, pass in 
+    body as the second argument. 
 
-    Use a .then to handle the promise returned from the axios call. Pass a callback function to the .then. Inside that callback, console log the res.data. 
+    Use a .then to handle the promise returned from the axios call. Pass a callback function to the .then. 
+    Inside that callback, console log the res.data. 
 
-    Based on what we did earlier to display this type of data, write code that will display the response in your HTML document. 
+    Based on what we did earlier to display this type of data, write code that will display the response 
+    in your HTML document. 
 */
 
 // CODE HERE 
